6626f512f44ddb953b2d8e73dc7f12b869d51b86
Ruiqi He <Ruiqi.He@uk.ey.com>
2019-02-28 14:39:35 +0000

    added basic kmeans clustering

--- preprocessing.py
+++ preprocessing.py
@@ -2,11 +2,13 @@ import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
+from nltk.cluster import KMeansClusterer
 import re
 from gensim.models import Word2Vec
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-
+from sklearn import cluster
+from sklearn import metrics
 
 data = pd.read_csv('tweets.csv')
 data['text'] = data['text'].astype(str)
@@ -22,13 +24,13 @@ def preprocessing(data):
     """
     data['text'] = data['text'].str.lower().str.split()
 
-    stop_words = stopwords.words('german')
+    #stop_words = stopwords.words('german')
 
     file = open('german_stopwords.txt', 'r')
     manual_stop_words = file.read()
 
     # remove stop words
-    data['text'] = data['text'].apply(lambda x: [item for item in x if item not in stop_words])
+    #data['text'] = data['text'].apply(lambda x: [item for item in x if item not in stop_words])
     data['text'] = data['text'].apply(lambda x: [item for item in x if item not in manual_stop_words])
 
     # remove special characters
@@ -46,45 +48,72 @@ def preprocessing(data):
     #data_copy['text'].to_csv('cleaned_tweets_only.csv', header=True, sep=';')
     return data
 
+
+def count_freq():
+    all_text = []
+    for num in range(len(data_clean['text'])):
+       all_text += data_clean['text'][num]
+
+    fdist1 = nltk.FreqDist(all_text)
+    print(fdist1.most_common(50))
+    fdist1.plot(50, cumulative=True)
+    plt.show()
+    pass
+
 data_clean = preprocessing(data)
 
-
-# count frequencies
-#all_text = []
-#for num in range(len(data_clean['text'])):
-#    all_text += data_clean['text'][num]
-
-#fdist1 = nltk.FreqDist(all_text)
-#print(fdist1.most_common(50))
-#fdist1.plot(50, cumulative=True)
-
-
-
 # get bag of words
-bag = []
-for tw in range(0, len(data_clean['text'])):
-    bag += data['text'][tw]
+def bagofwords(data_clean):
+    bag = []
+    for tw in range(0, len(data_clean['text'])):
+        bag += data['text'][tw]
+    return bag
 
 #word2vec model
-model = Word2Vec(bag, min_count=100)
+model = Word2Vec(bagofwords(data_clean), min_count=100)
 words = list(model.wv.vocab)
-
 
 X = model[model.wv.vocab]
 
 tsne = TSNE(n_components=2)
 tsne_results = tsne.fit_transform(X)
-print(tsne_results.shape)
 
-plt.scatter(tsne_results[:, 0], tsne_results[:, 1])
+
+# Clustering
+NUM_CLUSTERS = 10
+kclusterer = KMeansClusterer(NUM_CLUSTERS, distance=nltk.cluster.util.cosine_distance, repeats=25)
+assigned_clusters = kclusterer.cluster(X, assign_clusters=True)
+print(assigned_clusters)
+
+plt.scatter(tsne_results[:, 0], tsne_results[:, 1], c=assigned_clusters,)
 for i, word in enumerate(words):
     plt.annotate(word, xy=(tsne_results[i, 0], tsne_results[i, 1]))
 plt.show()
 
 
 
+# Additional information
 
+#for i, word in enumerate(words):
+#    print(word + ":" + str(assigned_clusters[i]))
 
+# kmeans = cluster.KMeans(n_clusters=NUM_CLUSTERS)
+# kmeans.fit(X)
 
+# labels = kmeans.labels_
+# centroids = kmeans.cluster_centers_
 
+# print("Cluster id labels for inputted data")
+# print(labels)
+# print("Centroids data")
+# print(centroids)
+#
+# print(
+#     "Score (Opposite of the value of X on the K-means objective which is Sum of distances of samples to their closest cluster center):")
+# print(kmeans.score(X))
+#
+# silhouette_score = metrics.silhouette_score(X, labels, metric='euclidean')
+#
+# print("Silhouette_score: ")
+# print(silhouette_score)
 
